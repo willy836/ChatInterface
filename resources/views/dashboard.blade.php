6707e70b6b28dbda646f69ee5bc51d23847ffb03
willy836 <wilsonkimiri836@gmail.com>
2023-10-23 16:44:21 +0300

    Chat UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div style="min-height: 60vh;" class="p-6 text-gray-900 dark:text-gray-100 relative">
+                    <div class="flex-grow"></div>
+                    <div class="absolute bottom-0 w-full">
+                        <form action="#" method="POST" class="flex gap-1">
+                            @csrf
+                            <div class="mb-3 w-5/6">
+                                <input class="text-black w-full" type="text">
+                            </div>
+                            <div>
+                                <button width="100" class="bg-indigo-800 rounded py-2 px-4" type="submit">Send</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
